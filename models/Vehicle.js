e4467542a66c4237944f0616e31576a5284b9017
Idrees Dargahwala <theiyd@Idreess-MacBook-Pro.local>
2020-01-16 15:43:30 +0530

    Remove unique license plate number

--- models/Vehicle.js
+++ models/Vehicle.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const vehicleSchema = new Schema({
   model: { type: String, required: true },
-  license_plate: { type: String, required: true, index: { unique: true } },
+  license_plate: { type: String, required: true },
   created_at: { type: Date, default: Date.now() }
 });
 
